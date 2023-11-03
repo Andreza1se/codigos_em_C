07ee781c820f3234465d28590c92049f27deaf78
Andreza1se <andreza.compt36@gmail.com>
2023-11-03 19:41:15 -0300

    novo commit

---
+++ pa.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int testa_PA(int n, int* v);
+
+int main(void)
+{
+    int n, PA, i;
+
+    printf("Coloque o valor de n:");
+    scanf("%d", &n);
+
+    int v[n];
+    printf("Insira as casas do vetor:");
+    for (i=0; i<n; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+
+    PA = testa_PA(n,v);
+
+    if(PA != 0)
+    {
+        printf("O valor %d\n", PA);
+    }
+    
+    return 0;
+}
+
+int testa_PA(int n, int* v)
+{
+    int k, i;
+    k = v[1] - v[0];
+
+    if(n<3)
+        {
+            return 0;
+        }
+    for (i=0; i<n; i++)
+    {
+        if(v[i] != v[0] + k*i)
+        {
+            printf("Nao e PA!!!");
+            return 0;
+        }
+    }
+    return k;
+}
